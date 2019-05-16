4c4482e2cdb828105bfc00a21bd33f2d1339943b
rafal-markowski <rafal_markowski@onet.pl>
2019-05-16 15:55:54 +0200

    Praca nad nawigacją

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title(); ?></title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/styles/bootstrap-4.3.1.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/styles/style.css">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header class="header">
+        <nav class="top-menu">
+            <div class="top-menu__wrapper container">
+                <a href="/" class="logo">
+                    <span class="bold">Money</span>
+                    Planet
+                </a>
+                <div class="top-menu__dropdown">
+                    <?php 
+                        wp_nav_menu(array(
+                            'theme_location' => 'header-menu',
+                            'container' => 'ul',
+                            'menu_class' => 'top-menu__ul'
+                        ));
+                    ?>
+                </div>
+                <button class="top-menu__btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
+        </nav>
+    </header>
---
+++ page-homepage.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: Strona główna
+*/
+get_header(); ?>
+
+<?php get_footer(); ?>
