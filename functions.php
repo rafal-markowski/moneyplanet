bf66f4747ef7985e06eae8b532d161eee0a5d2db
rafal-markowski <rafal_markowski@onet.pl>
2019-05-23 15:02:49 +0200

    Stworzenie widgetu

--- functions.php
+++ functions.php
@@ -9,3 +9,17 @@
         );
     }
     add_action('init', 'register_my_menus');
+
+    function theme_slug_widgets_init() {
+        register_sidebar( array(
+            'name' => _('Regulamin'),
+            'id' => 'regulations-widget-1',
+            'class' => 'regulations-widget',
+            'description' => '',
+            'before_widget' => '',
+            'after_widget' => '',
+            'before_title' => '<h2>',
+            'after_title' => '</h2>',
+        ) );
+    }
+    add_action( 'widgets_init', 'theme_slug_widgets_init');
